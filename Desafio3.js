ae2fadf5f83c27d60e14eb3b997406955c45563a
Everda <everda75@gmail.com>
2022-02-25 21:08:51 -0300

    arrglando desafio

--- Desafio3.js
+++ Desafio3.js
@@ -1,50 +1,43 @@
 const Desafio = (palabra, intervalo, cb) => {
     return new Promise((resolve) => {
-        
-            const palabraAR = palabra.split(' ');
-            let j = palabraAR.length;
-            let i = 0
-            let flag = false;
 
-            const recorrePalabra = () => {
-                if (i < j) {
-                    console.log(palabraAR[i]);
-                    i++;
-                } else {
-                    clearInterval(timer);
-                    cb();
-                    flag = true
-                }
+        const palabraAR = palabra.split(' ');
+        let j = palabraAR.length;
+        let i = 0
+
+
+        const recorrePalabra = () => {
+            if (i < j) {
+                console.log(palabraAR[i]);
+                i++;
+            } else {
+                clearInterval(timer);
+                cb();
+
             }
-            const timer = setInterval(() => { recorrePalabra(palabra) }, intervalo || 1000);
-            setTimeout(() => {
-                resolve([flag, j])
-            }, (intervalo * j) + 1000);
+        }
+        const timer = setInterval(() => { recorrePalabra(palabra) }, intervalo || 1000);
+        setTimeout(() => {
+            resolve(j)
+        }, (intervalo * j) + 2000);
 
-        
+
 
     }
     )
 }
 
 const procesar = async () => {
-    let wordcount = 0;
+    let wordCount = 0;
     let frases = ['esto es una prueba', 'Esta es otra prueba', 'Esta es la ultima prueba'];
     let intevalos = ['1000', '2000', '1000', '1000'];
-    let flag = false;
-    let resolve = [];
+
+
 
     for (let i = 0; i < frases.length; i++) {
-        if (!flag) {
-            resolve = await Desafio(frases[i], intevalos[i], () => { console.log('Termino la frase') });
-            flag = resolve[0];
-
-            wordcount += resolve[1];
-
-        }
-
+        wordCount += await Desafio(frases[i], intevalos[i], () => { console.log('Termino la frase') });
     }
-    console.log(wordcount);
+    console.log(wordCount);
     console.log("finProceso")
 }
 
