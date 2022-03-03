9c3c00dbb4ee38e8eab800d3829ca19d92f754b9
Everda <everda75@gmail.com>
2022-03-03 11:25:46 -0300

    desafio 4

--- desafio4-1.js
+++ desafio4-1.js
@@ -4,61 +4,61 @@
 
 // const { map, filter } = rxjs.operators;
 const { Observable } = rxjs;
+const { fromEvent } = rxjs;
+const { map, filter } = rxjs.operators;
+const { finalize } = rxjs.operators;
 
 
 //import { observable } from "rxjs";
 //const observable = require('rxjs').Observable;
 
 
-const input = document.getElementsByTagName('input')[0];
-
-addEventListener('keyup', (e) => {
-    const prueba = (valor) => {
-        return new Observable(observer => {
-            console.log(valor);
-            observer.next(valor);
-            if (valor == 'error') observer.error("devolvio error");
-            if (valor == 'complete') observer.complete("termino Complete");
+let text = document.getElementsByTagName('input')[0]
+let span = document.getElementsByTagName('span')[0]
+let leyenda = document.getElementsByTagName('p')[0]
 
 
-        })
+const input = fromEvent(text, 'keyup').pipe(
+    map(val => {
+        return val.target.value;
     }
-})
+    ),
+);
 
 
 
 
-        // const prueba = (valor) => {
 
 
-        //     return new Observable(observer => {
-        //         console.log(valor);
-        //         observer.next(valor);
-        //         if (valor == 'error') observer.error("devolvio error");
-        //         if (valor == 'complete') observer.complete("termino Complete");
+let observer = {
+    next: (valor) => {
+        if (valor === 'complete') {
+            console.log('complete');
+            text.value = "";
+            leyenda.innerHTML = "Completado por proceso";
+            subscription.unsubscribe(observer);
+        } else {
+            console.log("valor: ", valor);
+            span.innerHTML = valor.split('').reverse().join('');
+        }
+    },
+    error: (error) => {
+        console.log(error);
+        document.getElementsByTagName('input')[0].value = "";
+        leyenda.innerHTML = "Completado por error";
+    }
+}
 
-
-        //     })
-        // }
-
-
-
-        let observer = prueba(input.value).subscribe({
-            next: (valor) => {
-                console.log(valor);
-            },
-            error: (error) => {
-                console.log(error);
-            },
-            complete: () => {
-                console.log('complete');
-            }
-        })
+const subscription = input.subscribe(observer);
 
 
 
-// setTimeout(() => {
-//     observer.unsubscribe()
-// }, 5000);
+ setTimeout(() => {
+    subscription.unsubscribe()
+    document.getElementsByTagName('input')[0].value = "";
+    leyenda.innerHTML = "Completado por tiempo";
+    span.innerHTML="";
+
+ }, 5000);
 
 
