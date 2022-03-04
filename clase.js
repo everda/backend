43f2a3d1dfd5994248eeba022e66d19e66201efd
Enrique <everda75@gmail.com>
2022-03-03 23:02:18 -0300

    desafio 5

--- clase.js
+++ clase.js
@@ -1,12 +1,17 @@
 import http from 'http';
 
 const server = http.createServer((peticion,respuesta)=>{
-    let hora  = new Date().getHours();
-    respuesta.write(`Hora actual: ${hora}`)
+    // let hora  = new Date();
+    let randomNumber =Math.floor(Math.random()*10)
+    let objeto = {id: randomNumber ,title: "Producto " +randomNumber,
+    price: (Math.random()*10000).toFixed(2),
+    thumbnail: "Foto " + randomNumber}
+
+    respuesta.write(JSON.stringify(objeto));
     respuesta.end();
-    if(hora>=6&&hora<=12) respuesta.end("Buenos días");
-    if(hora>=13&&hora<=19) respuesta.end("Buenas tardes");
-    if(hora<=5||hora>=20) respuesta.end("Buenas noches");
+    // if(hora>=6&&hora<=12) respuesta.end("Buenos días");
+    // if(hora>=13&&hora<=19) respuesta.end("Buenas tardes");
+    // if(hora<=5||hora>=20) respuesta.end("Buenas noches");
 })
 server.listen(8080,()=>{
     console.log("Listening on port 8080")
