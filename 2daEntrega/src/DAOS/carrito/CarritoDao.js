795286f21f5d9d1d9b219197f71d1452cd7409dd
Everda <everda75@gmail.com>
2022-05-06 20:06:54 -0300

    asdasd

--- 2daEntrega/src/DAOS/carrito/CarritoDao.js
+++ 2daEntrega/src/DAOS/carrito/CarritoDao.js
@@ -1,27 +1,27 @@
-const CarritoDaoArchivo = require('./CarritoDaoArchivo')
-const CarritoDaoMongo = require('./CarritoDaoMongo')
-const CarritoDaoMemoria = require('./CarritoDaoMemoria')
-const CarritoDaoFirebase = require('./CarritoDaoFirebase')
 const config = require('./../../../config')
 
 
 
 if (config.database.DB === 'Mongo') {
+    const CarritoDaoMongo = require('./CarritoDaoMongo')
     console.log("loaded Mongo Cart")
 
     module.exports = CarritoDaoMongo
 }
 if (config.database.DB === 'Archivo') {
+    const CarritoDaoArchivo = require('./CarritoDaoArchivo')
     console.log("loaded Archivo Cart")
     module.exports = CarritoDaoArchivo
 }
 
 if (config.database.DB === 'Memoria') {
+    const CarritoDaoMemoria = require('./CarritoDaoMemoria')
     console.log("loaded Memoria Cart")
     module.exports = CarritoDaoMemoria
 }
 
 if (config.database.DB === 'Firebase') {
+    const CarritoDaoFirebase = require('./CarritoDaoFirebase')
     console.log("loaded Firebase Cart")
     module.exports = CarritoDaoFirebase
 }
